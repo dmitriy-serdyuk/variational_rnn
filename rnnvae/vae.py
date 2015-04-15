fd707cac0aa75cc9e84db1b4d3614288b90ee8d3
serdyuk <serdyuk.dmitriy@gmail.com>
2015-04-15 13:52:22 -0400

    Fixed entropy

--- rnnvae/vae.py
+++ rnnvae/vae.py
@@ -1,25 +1,23 @@
-from theano import tensor
+import argparse
+import numpy
+from theano import tensor, function
 
-from blocks.bricks import (MLP, Sigmoid, application, Brick, Random,
-                           Initializable)
+from blocks.bricks import (MLP, Sigmoid, application, Random,
+                           Initializable, Identity)
 from blocks.bricks.cost import CostMatrix
 from blocks.initialization import IsotropicGaussian, Constant
 from blocks.main_loop import MainLoop
 from blocks.extensions import FinishAfter, Timing, Printing
 from blocks.extensions.saveload import Dump, LoadFromDump
+from blocks.extensions.monitoring import (TrainingDataMonitoring,
+                                          DataStreamMonitoring)
 from blocks.algorithms import Scale, RMSProp, StepClipping, GradientDescent
 from blocks.model import Model
 from blocks.graph import ComputationGraph
 
 from fuel.datasets import MNIST
 from fuel.streams import DataStream
-
-
-class VAECost(CostMatrix):
-    @application(outputs=["cost"])
-    def apply(self, y, y_hat, mu, logsigma):
-        return self.cost_matrix(y, y_hat, mu, logsigma).sum(axis=1).mean()
-
+from fuel.schemes import ShuffledScheme, ConstantScheme
 
 
 class VAEEncoder(Random, Initializable):
@@ -34,18 +32,34 @@ class VAEEncoder(Random, Initializable):
     def apply(self, x):
         result = self.mlp.apply(x)
         mu, logsigma = tensor.split(result, [self.hidden_dim] * 2, 2, axis=1)
-        batch_size = result.shape[0]
-        epsilons = self.theano_rng.normal(0, 1, (batch_size, self.hidden_dim))
+        batch_size = x.shape[0]
+        epsilons = self.theano_rng.normal((batch_size, self.hidden_dim),
+                                          0., 1.)
         return mu, logsigma, mu + tensor.exp(logsigma) * epsilons
 
 
-class VariationalAutoEncoder(Initializable):
-    def __init__(self, input_dim, hidden_dim):
-        super(VariationalAutoEncoder, self).__init__()
+class VAEDecoder(Initializable):
+    def __init__(self, mlp, **kwargs):
+        super(VAEDecoder, self).__init__(**kwargs)
+        self.mlp = mlp
+        self.children = [self.mlp]
 
-        encoder_mlp = MLP([Sigmoid(), Sigmoid()], [input_dim, 500, None])
+    @application
+    def apply(self, z):
+        return self.mlp.apply(z)
+
+
+class VariationalAutoEncoder(Initializable):
+    def __init__(self, input_dim, hidden_dim, **kwargs):
+        super(VariationalAutoEncoder, self).__init__(**kwargs)
+
+        encoder_mlp = MLP([Sigmoid(), Identity()],
+                          [input_dim, 101, None])
+        decoder_mlp = MLP([Sigmoid(), Sigmoid()],
+                          [hidden_dim, 101, input_dim])
+        self.hidden_dim = hidden_dim
         self.encoder = VAEEncoder(encoder_mlp, hidden_dim)
-        self.decoder = MLP([Sigmoid(), Sigmoid()], [hidden_dim, 500, input_dim])
+        self.decoder = VAEDecoder(decoder_mlp)
         self.children = [self.encoder, self.decoder]
 
     @application
@@ -55,49 +69,109 @@ class VariationalAutoEncoder(Initializable):
         return mu, logsigma, x_hat
 
     @application
+    def sample(self, batch_size):
+        z = self.encoder.theano_rng.normal((batch_size, self.hidden_dim),
+                                           0., 1.)
+        return self.decoder.apply(z)
+
+    @application
     def cost(self, x):
         mu, logsigma, x_hat = self.apply(x)
         return (self.regularization_cost(mu, logsigma) +
-                self.reconstruction_cost(x, x_hat))
+                self.reconstruction_cost(x, x_hat)).mean()
 
     @application
     def regularization_cost(self, mu, logsigma):
-        return 0.5 * ((1 + 2 * logsigma) - mu ** 2 -
-                      tensor.exp(logsigma) ** 2).sum(axis=1)
+        return - 0.5 * ((1 + 2 * logsigma) - mu ** 2 -
+                        tensor.exp(logsigma) ** 2).sum(axis=1)
 
     @application
     def reconstruction_cost(self, x, x_hat):
-        return tensor.nnet.categorical_crossentropy(x_hat, x)
+        return tensor.nnet.binary_crossentropy(x_hat, x).sum(axis=1)
 
 
-def main():
+def main(save, load, **kwargs):
     input_dim = 784
-    hidden_dim = 20
+    hidden_dim = 10
+    batch_size = 100
 
-    x = tensor.matrix('x')
+    features = tensor.matrix('features')
 
-    vae = VariationalAutoEncoder(input_dim, hidden_dim)
+    vae = VariationalAutoEncoder(input_dim, hidden_dim,
+                                 weights_init=IsotropicGaussian(0.01),
+                                 biases_init=Constant(0.))
+    vae.initialize()
 
-    mu, logsigma, x_hat = vae.apply(x)
+    mu, logsigma, x_hat = vae.apply(features)
+    cost = vae.cost(features)
+    cost.name = 'cost'
+    regularization_cost = vae.regularization_cost(mu, logsigma).mean()
+    regularization_cost.name = 'regularization_cost'
+    reconstruction_cost = vae.reconstruction_cost(features, x_hat).mean()
+    reconstruction_cost.name = 'reconstruction_cost'
 
-    cost = VAECost().apply(x, x_hat, mu, logsigma)
-
-    cg = ComputationGraph(cost)
+    cg = ComputationGraph([cost, reconstruction_cost, regularization_cost])
     model = Model(cost)
 
-    algorithm = GradientDescent(step_rule=Scale(1e-4), params=cg.parameters,
+    algorithm = GradientDescent(step_rule=RMSProp(1e-4), params=cg.parameters,
                                 cost=cost)
 
-    extensions = [Dump('VAE'), Timing(), Printing(),
-                  FinishAfter(after_n_epochs=10)]
+    extensions = []
+    if load:
+        extensions.append(LoadFromDump('VAE'))
+    if save:
+        extensions.append(Dump('VAE', after_epoch=True))
+    extensions.append(FinishAfter(after_n_epochs=6001))
 
-    train_dataset = MNIST('train', binary=True)
-    train_stream = DataStream(train_dataset)
+    train_dataset = MNIST('train', binary=True, sources=('features',))
+    train_stream = DataStream(train_dataset,
+                              iteration_scheme=ShuffledScheme(
+                                  examples=train_dataset.num_examples,
+                                  batch_size=batch_size))
+    train_monitor = TrainingDataMonitoring(
+        [cost, regularization_cost, reconstruction_cost],
+        prefix='train', after_epoch=True)
+
+    test_dataset = MNIST('test', binary=True, sources=('features',))
+    test_stream = DataStream(test_dataset,
+                             iteration_scheme=ShuffledScheme(
+                                 examples=test_dataset.num_examples,
+                                 batch_size=batch_size))
+    test_monitor = DataStreamMonitoring([cost], test_stream, prefix='test')
+    extensions.extend([train_monitor, test_monitor])
+    extensions.extend([Timing(), Printing()])
     main_loop = MainLoop(model=model, algorithm=algorithm,
                          data_stream=train_stream,
                          extensions=extensions)
     main_loop.run()
 
+    num_samples = 10
+    samples = vae.sample(num_samples)
+    samples = function([], samples)()
+    from matplotlib import pyplot as plt
+    sample = numpy.zeros((28, 0))
+    for i in xrange(num_samples):
+        sample = numpy.concatenate([sample, samples[i].reshape((28, 28))], axis=1)
+    plt.imshow(sample)
+    plt.show()
+    f = function([features], x_hat)
+    for data in train_stream.get_epoch_iterator():
+        data_hat = f(data[0])
+        break
+    for image in data_hat:
+        plt.imshow(image.reshape((28, 28)))
+        plt.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', action='store_true', default=False,
+                        help='Save model')
+    parser.add_argument('--load', action='store_true', default=False,
+                        help='Load model')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(**args.__dict__)
